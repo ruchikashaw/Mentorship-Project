b671b7dcf45a7fa5b2d3993b3110dd1fa3878699
ruchikashaw <ruchikashaw069@gmail.com>
2022-01-23 15:49:39 +0530

    Proper Climate Images added

--- script.js
+++ script.js
@@ -37,27 +37,27 @@ const getWeather=async (city)=>
         tempvalue.textContent=Math.round(feels_like-273);
         if(id<300 && id>200)
         {
-            tempicon.src="./icons/thunderstorm.svg"
+            tempicon.src="./icons/thunderstorm.png"
         }
        else  if(id<400 && id>300)
         {
-            tempicon.src="./icons/cloud-solid.svg"
+            tempicon.src="./icons/sun-cloud.png"
         }
        else if(id<600&& id>500)
         {
-            tempicon.src="./icons/rain.svg"
+            tempicon.src="./icons/rain.png"
         }
        else  if(id<700 && id>600)
         {
-            tempicon.src="./icons/snow.svg"
+            tempicon.src="./icons/snow.png"
         }
        else  if(id<800 && id>700)
         {
-            tempicon.src="./icons/clouds.svg"
+            tempicon.src="./icons/cloud big.png"
         }
          else if(id==800)
         {
-            tempicon.src="./icons/clouds-and-sun.svg"
+            tempicon.src="./icons/cloudy_day.png"
         }
 
 
